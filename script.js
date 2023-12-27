d3ac59960edef6a5245fd442bb0fffb9fef86269
githubGuestGuy <154698207+githubGuestGuy@users.noreply.github.com>
2023-12-27 11:10:37 +0400

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("test").addEventListener("click", function() {
-  alert("i dont even know how to code in js u actually expected something to happen lmao, anyways thanks for trying out my 'website'");
-});
